548dbfbe7c359bbcdb183dd7094050712833550d
unknown <E598211@CH3ULT59N2HV1.global.ds.honeywell.com>
2014-03-26 14:11:27 +0800

    init code

---
+++ Cmsl.cpp
@@ -0,0 +1,36 @@
+// Cmsl.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "CXml/xml.h"
+
+using namespace JWXml;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	CoInitializeEx(NULL,COINIT_MULTITHREADED);
+
+	CXml xml;
+	xml.Open(L"\\DeviceAPI.xml");
+	CXmlNodePtr root = xml.GetRoot();
+	CXmlNode nodeRoot(root);
+
+	CString name = nodeRoot.GetName();
+
+	if(nodeRoot.HasChildren())
+	{
+		CString str("");
+		nodeRoot.
+		str = nodeRoot.GetAttribute(_T("desc"), str);
+		int xj = nodeRoot.GetAttribute(_T("id"), 2);
+		CXmlNodesPtr nodesPtr = nodeRoot.GetChildren();
+		long nodesCount = nodesPtr->GetCount();
+	}
+
+	OutputDebugString(name);
+
+	xml.Close();
+	CoUninitialize();
+	return 0;
+}
+
